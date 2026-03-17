30820954682c91c4155cabd595dcbeb4513079d2
Johan Lindell <johan.lindell91@gmail.com>
2026-03-17 20:49:37 +0100

    adds hover and active state to navbar in header

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const toggle = document.querySelector(".menu-toggle");
 toggle.addEventListener("click", () => {
   header.classList.toggle("menu-open");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const currentLocation = location.href;
+  const menuItems = document.querySelectorAll(".header__navbar-link");
+
+  menuItems.forEach((item) => {
+    if (item.href === currentLocation) {
+      item.classList.add("active");
+    }
+  });
+});
