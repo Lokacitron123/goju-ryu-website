e70af7edba04ace6438166842ca8209221b482c7
Johan Lindell <johan.lindell91@gmail.com>
2026-03-21 21:29:36 +0100

    Initial  commit version 2.0

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const header = document.querySelector(".header__main");
-const toggle = document.querySelector(".menu-toggle");
+const header = document.querySelector(".header");
+const toggle = document.querySelector(".nav__mobile-toggle");
+const menu = document.querySelector(".nav__menu");
 
 toggle.addEventListener("click", () => {
-  header.classList.toggle("menu-open");
+  // Toggle the open state on the button
+  toggle.classList.toggle("nav__mobile-toggle--open");
+
+  // Toggle the menu visibility
+  menu.classList.toggle("nav__menu--open");
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   const currentLocation = location.href;
-  const menuItems = document.querySelectorAll(".header__navbar-link");
+  const menuItems = document.querySelectorAll(".nav__link");
 
   menuItems.forEach((item) => {
     if (item.href === currentLocation) {
